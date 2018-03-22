183df31f09a40b9ef3b837981f49f4027699afc6
Ilmari Heiskanen <ilmari_heiskanen@hotmail.com>
2018-03-22 19:51:48 +0200

    asd

--- KysymysGeneraattori/src/main/java/Main/KysymysGeneraattoriGUI.java
+++ KysymysGeneraattori/src/main/java/Main/KysymysGeneraattoriGUI.java
@@ -7,11 +7,15 @@ package Main;
 
 import Dao.Database;
 import UI.Login;
+import UI.LoginForGUI;
 import java.sql.SQLException;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
@@ -25,17 +29,65 @@ public class KysymysGeneraattoriGUI extends Application{
 
     
     @Override
-    public void start(Stage ikkuna) {
+    public void start(Stage ikkuna) throws ClassNotFoundException {
+        Database users = new Database("jdbc:sqlite:users.db");
+        LoginForGUI kirjautuminen = new LoginForGUI(users);
         
-        Button nappi = new Button("Tämä on nappi");
-
-        BorderPane komponenttiryhma = new BorderPane();
-        komponenttiryhma.setLeft(nappi);
-        //komponenttiryhma.setRight(taulu);
-
-        Scene nakyma = new Scene(komponenttiryhma, 640, 480);
+        Label nameText = new Label("Nimi: ");
+        TextField nameInput = new TextField();
+        Label passText = new Label("Salasana: ");
+        TextField passInput = new TextField();
         
-        ikkuna.setScene(nakyma);
+        Button newUser = new Button("Uusi käyttäjä");
+        Button existingUser = new Button("Vanha käyttäjä");
+        Button hiScores = new Button("Tuloslista");
+        Button quit = new Button("Lopeta");
+        Button backToLogin = new Button("Kirjautumiseen");
+        
+        GridPane loginGUI = new GridPane();
+        GridPane hiScoreGUI = new GridPane();
+        
+        loginGUI.add(nameText, 0, 0);
+        loginGUI.add(nameInput, 1, 0);
+        loginGUI.add(passText, 0, 1);
+        loginGUI.add(passInput, 1, 1);
+        loginGUI.add(newUser, 0, 2);
+        loginGUI.add(existingUser, 1, 2);
+        loginGUI.add(hiScores, 0, 3);
+        loginGUI.add(quit, 1, 3);
+        hiScoreGUI.add(backToLogin, 0, 0);
+        
+        loginGUI.setHgap(10);
+        loginGUI.setVgap(10);
+        loginGUI.setPadding(new Insets(10, 10, 10, 10));
+        
+        
+        Scene hiScoreNakyma = new Scene(hiScoreGUI);
+        Scene loginNakyma = new Scene(loginGUI);
+        
+        
+        
+        quit.setOnAction((event) -> {
+            ikkuna.close();
+        });
+        hiScores.setOnAction((event) -> {
+            ikkuna.setTitle("HiScores");
+            ikkuna.setScene(hiScoreNakyma);
+        });
+        newUser.setOnAction((event) -> {
+            
+        });
+        existingUser.setOnAction((event) -> {
+            
+        });
+        backToLogin.setOnAction((event) -> {
+            ikkuna.setScene(loginNakyma);
+        });
+        
+
+        
+        ikkuna.setTitle("Login");
+        ikkuna.setScene(loginNakyma);
         ikkuna.show();
         
     }
---
+++ KysymysGeneraattori/src/main/java/UI/LoginForGUI.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import Dao.Database;
+import Dao.UsersDao;
+import Main.KysymysGeneraattoriGUI;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+import static javafx.application.Application.launch;
+
+/**
+ *
+ * @author ilmar
+ */
+public class LoginForGUI {
+    
+    Database database;
+    Scanner lukija;
+    UsersDao usersDao;
+    
+    
+
+    public LoginForGUI(Database users) {
+        this.database = users;
+        this.lukija = new Scanner(System.in);
+        this.usersDao = new UsersDao(this.database);
+    }
+
+    
+    public void tulostaUsers() throws SQLException {
+        this.usersDao.printUsers();
+        this.aloita();
+    }
+    
+    public void aloita() throws SQLException {
+        System.out.println("Login: ");
+        System.out.println("1. Vanha käyttäjä");
+        System.out.println("2. Uusi käyttäjä");
+        System.out.println("3. Tulosta pisteet");
+        System.out.println("4. Lopeta");
+        System.out.println("5. GUI");
+        int valinta = Integer.parseInt(lukija.nextLine());
+        if (valinta == 1) {
+            login();
+        }
+        if (valinta == 3) {
+            tulostaUsers();
+        }
+        if (valinta == 5) {
+            launch(KysymysGeneraattoriGUI.class);
+        }
+    }
+    
+    public void login() throws SQLException { //logiikka sisäänkirjautumiseen, tarkistaa databasesta tiedot
+        System.out.print("Käyttäjänimi: ");
+        String username = lukija.nextLine();
+        System.out.print("Salasana: ");
+        String password = lukija.nextLine();
+        if (username.equals("admin") && password.equals("admin")) { //HUOM! Tämä if looppi vain kehitysvaiheessa! Poistetaan lopullsesta
+            System.out.println("ADMIN COMMANDS: ");
+            System.out.println("1. Clear database");
+            System.out.println("2. Back to login");
+            int valinta = Integer.parseInt(lukija.nextLine());
+            if (valinta == 2) {
+                aloita();
+            } if (valinta == 1) {
+                clearDatabase();
+            }
+        } else {
+            System.out.println("KESKEN"); //Tähän varsinainen sisäänkirjautuminen
+        }   
+    }
+    
+    
+    public void clearDatabase() throws SQLException {
+        this.usersDao.clearDatabase();
+        aloita();
+    }
+    
+
+    
+}
